9d44f00351d5b5c4d4ce1742e0ac8d797d9ddaf6
litov74 <73787076+litov74@users.noreply.github.com>
2020-11-07 02:49:01 +0500

    Update fwdBwdForFourthText.py

--- keyboards/inline/fwdBwdForFourthText.py
+++ keyboards/inline/fwdBwdForFourthText.py
@@ -2,7 +2,7 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 choice = InlineKeyboardMarkup(row_width=1)
 
-c1 = InlineKeyboardButton(text="⬅ К предыдущему примеру", callback_data="toText:3")
+c1 = InlineKeyboardButton(text="⬅ К предыдущему примеру", callback_data="toText:2")
 choice.insert(c1)
 c2 = InlineKeyboardButton(text="К меню клиентского чата", callback_data="backToClientsMenu:0")
 choice.insert(c2)
