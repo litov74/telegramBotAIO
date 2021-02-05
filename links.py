bd24425064217c53e2cd77620c8a4a3858a3ad38
litov74 <73787076+litov74@users.noreply.github.com>
2021-02-05 08:14:02 +0500

    Update links.py

--- links.py
+++ links.py
@@ -14,6 +14,6 @@ howToTrackAnOrderLink = "https://youtu.be/VNFJKamFZGs" #added to page 2
 howToActivateFirstContractLink = "https://youtu.be/m1IhSIFdllA" #added to menu
 howToGetAPurchaseReportLink = "https://youtu.be/8psm75UAs7c" #added to page 2
 howToContactUsLink = "https://youtu.be/HtIkgBRH9_E" #added to page 2
-marketingLink = "https://youtu.be/zX73IX3fTqI" #added to page 2
+marketingLink = "https://youtu.be/w59pzTVGVdI" #added to page 2
 teamSystemLink = "https://youtu.be/69gS3IUwMCM" #added to page 1
 howToMakeCallLink = "https://youtu.be/J5NAEO2-Lhc" #added to page 2
